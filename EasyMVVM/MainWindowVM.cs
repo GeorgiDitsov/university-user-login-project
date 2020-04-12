b07cd927bafaa65edb1029e0e0dcfca4673e4677
Georgi Ditsov <georgi.ditsov@outlook.com>
2020-04-13 00:32:27 +0300

    MVVM initialization

---
+++ EasyMVVM/MainWindowVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace EasyMVVM
+{
+    public class MainWindowVM : DependencyObject, INotifyPropertyChanged
+    {
+        private ObservableCollection<string> _BackingProperty;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<string> BoundProperty
+        {
+            get { return _BackingProperty; }
+            set { _BackingProperty = value; PropChanged("BoundProperty"); }
+        }
+
+        public MainWindowVM()
+        {
+            Model m = new Model();
+            BoundProperty = m.GetData();
+        }
+
+        public void PropChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ WpfExample/MainWindowViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfExample
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ICommand hiButtonCommand;
+        private ICommand toggleExecuteCommand
+        { get; set; }
+        private bool canExecute = true;
+        private string lblHi = "";
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HiLabelContent 
+        {
+            get { return lblHi; }
+            set 
+            {
+                if (lblHi != value) 
+                {
+                    this.lblHi = value;
+                    PropChanged("lblHi");
+                }
+            }
+        }
+        public string HiButtonContent
+        {
+            get { return "click to hi"; }
+        }
+        public string CurrentDateTime
+        {
+            get { return DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString(); }
+        }
+        public bool CanExecute
+        {
+            get { return this.canExecute; }
+            set
+            {
+                if (this.canExecute == value)
+                { return; }
+                this.canExecute = value;
+            }
+        }
+        public ICommand ToggleExecuteCommand
+        {
+            get
+            { return toggleExecuteCommand; }
+            set
+            { toggleExecuteCommand = value; }
+        }
+        public ICommand HiButtonCommand
+        {
+            get
+            { return hiButtonCommand; }
+            set
+            { hiButtonCommand = value; }
+        }
+        public void PropChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public MainWindowViewModel()
+        {
+            HiButtonCommand = new RelayCommand(ShowMessage, param => this.canExecute);
+            toggleExecuteCommand = new RelayCommand(ChangeCanExecute);
+        }
+
+        public void ShowMessage(object obj)
+        {
+            HiLabelContent = obj.ToString();
+        }
+        public void ChangeCanExecute(object obj)
+        {
+            canExecute = !canExecute;
+        }
+    }
+}
